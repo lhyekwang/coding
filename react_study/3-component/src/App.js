bfdc62868b744b8ac57398a0829240e46fedb41c
bean.zino <bean.zino@kakaocorp.com>
2022-01-15 11:21:34 +0900

    검색결과

--- react_study/3-component/src/App.js
+++ react_study/3-component/src/App.js
@@ -1,21 +1,38 @@
 import React from "react" ;
 import Header from "./components/Header.js"
 import SearchForm from "./components/SearchForm.js"
+import SearchResult from "./components/SearchResult.js"
+import store from "./Store.js"
 
 export default class App extends React.Component {
 
   constructor(){
     super();
 
-    this.state = { searchKeyword : ''};
+    this.state = { 
+      searchKeyword : '', 
+      searchResult : [], 
+      submitted : false,
+    };
   }
 
   search(searchKeyword){
-    console.log( "TODO : search" ,  searchKeyword);
+    // console.log( "TODO : search" ,  searchKeyword);
+    const searchResult = store.search(searchKeyword);
+    this.setState({
+      searchKeyword,
+      searchResult,
+      submitted : true
+    })
   }
 
   handleReset(){
-    console.log('HandelReset');
+    // console.log('HandelReset');
+    this.setState( { 
+      searchKeyword : '', 
+      searchResult : [], 
+      submitted : false
+    })
   }
 
   handleChangeInput(searchKeyword){
@@ -26,16 +43,20 @@ export default class App extends React.Component {
   }
 
   render(){
+    const { searchKeyword , searchResult, submitted} = this.state;
     return(
       <>
         <Header title="검색"/>
         <div className="container">
           <SearchForm 
-            value = {this.state.searchKeyword}
+            value = {searchKeyword}
             onChange={(value) => this.handleChangeInput(value)}
-            onSubmit ={ (searchKeyword) => this.search(searchKeyword)} 
+            onSubmit ={ () => this.search(searchKeyword)} 
             onReset = { () => this.handleReset() }
           />
+          <div className="content">
+            {submitted &&<SearchResult data = { searchResult }/>}
+          </div>
         </div>
       </>
     );
