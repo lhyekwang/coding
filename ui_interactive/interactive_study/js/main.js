c7cf8a4c60edf22731cff0b5f74c1102e824cf5b
bean.zino <bean.zino@kakaocorp.com>
2022-04-01 12:35:07 +0900

    셋팅 및 환경설정

---
+++ ui_interactive/interactive_study/js/main.js
@@ -0,0 +1,64 @@
+( ()=>{
+  
+  let yOffset = 0;
+  const sceneInfo = [
+    {
+      // 0
+      type : 'sticky',
+      heightNum:5, //브라우저 높이의 5배로 scrolllHeight셋팅
+      scrollHeight : 0, 
+      objs: {
+        container : document.querySelector('#scroll-section-0')
+      }
+    }, 
+    {
+      // 1
+      type : 'normal',
+      heightNum:5, //브라우저 높이의 5배로 scrolllHeight셋팅
+      scrollHeight : 0, 
+      objs: {
+        container : document.querySelector('#scroll-section-1')
+      }
+    }, 
+    {
+      // 2
+      type : 'sticky',
+      heightNum:5, //브라우저 높이의 5배로 scrolllHeight셋팅
+      scrollHeight : 0, 
+      objs: {
+        container : document.querySelector('#scroll-section-2')
+      }
+    }, 
+    {
+      // 3
+      type : 'sticky',
+      heightNum:5, //브라우저 높이의 5배로 scrolllHeight셋팅
+      scrollHeight : 0, 
+      objs: {
+        container : document.querySelector('#scroll-section-3')
+      }
+    }  
+  ];
+
+    
+  function setLayout(){
+    //  걱 스크롤 섹션 높이 셋팅
+    for(let i=0 ; i<sceneInfo.length ;i ++){
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum *  window.innerHeight;
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }    
+  }
+
+  function scrollLoop(){
+    console.log(yOffset);
+  }
+
+  window.addEventListener('resize' , setLayout);
+  window.addEventListener('scroll' , ()=>{
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  })
+  setLayout();
+
+
+})();
